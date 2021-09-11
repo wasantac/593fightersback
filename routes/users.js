87b12e15aa07bb5ca02f481236bd260c4891ed37
wasantac <34143200+wasantac@users.noreply.github.com>
2021-09-10 20:35:15 -0500

    [

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,9 @@ var router = express.Router();
 var userController = require('../controllers/user.controller');
 const passport = require("passport");
 /* GET users listing. */
-router.get('/',userController.findAll);
+router.get('/',passport.authenticate('jwt-admin',{session:false}),userController.findAll);
 router.post('/',userController.create);
-router.delete('/:id',userController.deleteID);
+router.delete('/:id',passport.authenticate('jwt-admin',{session:false}),userController.deleteID);
 router.get('/id',passport.authenticate('jwt',{session:false}),userController.findID);
 router.put('/',passport.authenticate('jwt',{session:false}),userController.findIDandUpdate)
 
